0bfa40a380fddde7f33a7287ca8990bb7f9c3253
PeymanNr <peyman.naseri7@gmail.com>
2024-09-22 15:35:03 +0330

    Added role field to MyUser model to distinguish user type.

--- account/api/serializers.py
+++ account/api/serializers.py
@@ -5,10 +5,13 @@ from account.models import MyUser
 class AuthSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField()
+    role = serializers.CharField()
 
     def validate(self, data):
         email = data.get('email')
         password = data.get('password')
+        role = data.get('role')
+
 
         if not email or not password:
             raise serializers.ValidationError(
@@ -17,8 +20,11 @@ class AuthSerializer(serializers.Serializer):
             user = MyUser.objects.get(email=email)
             if not user.check_password(password):
                 raise serializers.ValidationError('Incorrect password.')
+            if user.role != role:
+                raise serializers.ValidationError('Already registered with this email.')
         except MyUser.DoesNotExist:
-            user = MyUser.objects.create_user(email=email, password=password)
+            user = MyUser.objects.create_user(email=email,
+                                              password=password, role=role)
 
         data['user'] = user
         return data
